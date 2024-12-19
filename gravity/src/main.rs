89ef33de185f47614988a8981a17754e350a1d05
Rui Valim <r.valim.junior@gmail.com>
2024-12-19 11:23:10 -0300

    UI

--- gravity/src/main.rs
+++ gravity/src/main.rs
@@ -1,5 +1,9 @@
 mod physics;
 
+use ggegui::{
+    egui::{self, RichText},
+    Gui,
+};
 use ggez::{
     conf::WindowMode,
     event::{self, EventHandler},
@@ -7,13 +11,15 @@ use ggez::{
     graphics::{self, Canvas, Color, DrawParam, Mesh},
     GameResult,
 };
-use rand::random;
+use rand::{random, rngs::ThreadRng, thread_rng, Rng};
 use rapier2d::prelude::*;
 
 struct MainState {
     balls: Vec<(Mesh, RigidBodyHandle)>,
     floors: Vec<(Mesh, RigidBodyHandle, Vec2)>,
     physics: physics::Physics,
+    gui: Gui,
+    rng: ThreadRng,
 }
 
 impl MainState {
@@ -86,10 +92,14 @@ impl MainState {
             true,
         ));
 
+        let gui = Gui::new(ctx);
+
         Ok(MainState {
             balls,
             floors,
             physics,
+            gui,
+            rng: thread_rng(),
         })
     }
 
@@ -108,17 +118,48 @@ impl EventHandler for MainState {
         _x: f32,
         _y: f32,
     ) -> Result<(), ggez::GameError> {
-        let new_floor =
-            self.physics
-                .new_cuboid(_ctx, 100.0, 100.0, _x, _y, Color::WHITE, 0.7, true);
+        // let new_floor =
+        //     self.physics
+        //         .new_cuboid(_ctx, 100.0, 100.0, _x, _y, Color::WHITE, 0.7, true);
 
-        self.floors.push(new_floor);
+        //self.floors.push(new_floor);
 
         Ok(())
     }
 
-    fn update(&mut self, _ctx: &mut ggez::Context) -> GameResult {
+    fn update(&mut self, ctx: &mut ggez::Context) -> GameResult {
         self.physics.step();
+        let gui_ctx = self.gui.ctx();
+
+        egui::Window::new("UI").show(&gui_ctx, |ui| {
+            if ui.button(RichText::new("EXPLODE!!").size(40.0)).clicked() {
+                for ball in &self.balls {
+                    let force = 10000000.0;
+                    let rand_x = (random::<f32>() * force) * if random() { 1.0 } else { -1.0 };
+                    let rand_y = (random::<f32>() * force) * if random() { 1.0 } else { -1.0 };
+                    self.physics.rigid_body_set[ball.1]
+                        .apply_impulse(Vector::new(rand_x, rand_y), true);
+                }
+            }
+            if ui.button(RichText::new("New Ball").size(40.0)).clicked() {
+                let ball_init_x = random::<f32>() * 2000.0;
+                let ball_init_y = random::<f32>() * 1000.0;
+                let ball = self.physics.new_ball(
+                    ctx,
+                    ball_init_x.clamp(120.0, 2000.0 - 120.0),
+                    ball_init_y.clamp(120.0, 1000.0 - 120.0),
+                    20.0,
+                    Color::WHITE,
+                    0.7,
+                    false,
+                );
+                self.balls.push(ball);
+            }
+            if ui.button(RichText::new("Quit").size(40.0)).clicked() {
+                ctx.request_quit();
+            }
+        });
+        self.gui.update(ctx);
 
         Ok(())
     }
@@ -140,6 +181,7 @@ impl EventHandler for MainState {
                 DrawParam::default().dest(Vec2::new(floor.2.x, floor.2.y)),
             );
         }
+        canvas.draw(&self.gui, DrawParam::default().dest(Vec2::ZERO));
 
         self.draw_gizmos(&ctx, &mut canvas);
 
