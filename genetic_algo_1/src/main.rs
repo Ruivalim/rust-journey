bdb1d0731a58d4c48a53a139c67eec1d1fc8a8b5
Rui Valim <r.valim.junior@gmail.com>
2025-01-26 18:16:39 -0300

    reproduction and gizmos

--- genetic_algo_1/src/main.rs
+++ genetic_algo_1/src/main.rs
@@ -13,14 +13,16 @@ mod helpers;
 
 const MOVEMENT_COST: f32 = 0.05;
 const ATTACK_COST: f32 = 15.;
-const MAP_HEIGHT: f32 = 1500.0;
-const MAP_WIDTH: f32 = 1500.0;
+const MAP_HEIGHT: f32 = 800.0;
+const MAP_WIDTH: f32 = 800.0;
 
 #[derive(Resource)]
 pub struct RandomSource(ChaCha12Rng);
 
 #[derive(Component, Clone, Copy)]
 pub struct Cell {
+    parent_1: Option<Uuid>,
+    parent_2: Option<Uuid>,
     id: Uuid,
     pos_x: f32,
     pos_y: f32,
@@ -32,6 +34,8 @@ pub struct Cell {
     attack: f32,
     target_location: Option<Vec2>,
     movement_probability: f64,
+    offspring_probability: f64,
+    family_color: Color,
 }
 
 #[derive(Resource, Default)]
@@ -66,6 +70,8 @@ fn main() {
                 get_cell_info.run_if(input_pressed(MouseButton::Left)),
                 update_cell_info,
                 cells_eat,
+                cell_reproduction,
+                draw_gismos,
             ),
         )
         .run();
@@ -80,7 +86,7 @@ fn setup(
 
     commands.spawn((Camera2d::default(), MainCamera));
 
-    for _ in 0..30 {
+    for _ in 0..100 {
         let x = seeded_rng.gen_range(-MAP_WIDTH / 2.0..MAP_WIDTH / 2.0);
         let y = seeded_rng.gen_range(-MAP_HEIGHT / 2.0..MAP_HEIGHT / 2.0);
         commands.spawn((
@@ -91,27 +97,36 @@ fn setup(
         ));
     }
 
-    for _ in 0..5 {
+    for _ in 0..15 {
         let x = seeded_rng.gen_range(-MAP_WIDTH / 2.0..MAP_WIDTH / 2.0);
         let y = seeded_rng.gen_range(-MAP_HEIGHT / 2.0..MAP_HEIGHT / 2.0);
         let width = seeded_rng.gen_range(15.0..25.0);
         let height = seeded_rng.gen_range(15.0..25.0);
+        let family_color = Color::linear_rgb(
+            seeded_rng.gen_range(0.0..1.0),
+            seeded_rng.gen_range(0.0..1.0),
+            seeded_rng.gen_range(0.0..1.0),
+        );
         commands.spawn((
             Mesh2d(meshes.add(Rectangle::new(width, height))),
-            MeshMaterial2d(materials.add(Color::from(RED))),
+            MeshMaterial2d(materials.add(Color::from(family_color))),
             Transform::default().with_translation(Vec3::new(x, y, 0.0)),
             Cell {
+                parent_1: None,
+                parent_2: None,
                 id: Uuid::new_v4(),
                 pos_x: x,
                 pos_y: y,
                 width,
                 height,
                 health: 50.0,
-                movement_speed: seeded_rng.gen_range(0.15..100.0),
-                vision_range: seeded_rng.gen_range(0.15..100.0),
+                movement_speed: seeded_rng.gen_range(15.0..100.0),
+                vision_range: seeded_rng.gen_range(15.0..100.0),
                 attack: 10.0,
                 target_location: None,
                 movement_probability: seeded_rng.gen(),
+                offspring_probability: seeded_rng.gen_range(0.1..0.5),
+                family_color,
             },
         ));
     }
@@ -125,7 +140,7 @@ fn move_cells(
     time: Res<Time>,
     mut random_source: ResMut<RandomSource>,
 ) {
-    let rng = &mut random_source.0; // Use the random source directly
+    let rng = &mut random_source.0;
     for (mut transform, mut cell) in cell_query.iter_mut() {
         if rng.gen_bool(cell.movement_probability) {
             continue;
@@ -211,8 +226,9 @@ fn ui_system(mut contexts: EguiContexts, selected_cell: Res<CellSelected>) {
             ui.label(format!("Health: {}", cell.health));
             ui.label(format!("Pos X: {}", cell.pos_x));
             ui.label(format!("Pox Y: {}", cell.pos_y));
-            ui.label(format!("Target: {:?}", cell.target_location));
             ui.label(format!("Speed: {}", cell.movement_speed));
+            ui.label(format!("Parent 1: {:?}", cell.parent_1));
+            ui.label(format!("Parent 2: {:?}", cell.parent_2));
             ui.label(format!(
                 "Probability Movement: {}",
                 cell.movement_probability
@@ -282,6 +298,103 @@ fn cells_eat(
                 cell.0.health += 15.0;
                 commands.entity(foods.2).despawn();
             }
+        }
+    }
+}
+
+fn cell_reproduction(
+    mut commands: Commands,
+    mut cell_query: Query<(&mut Cell, &Transform, Entity)>,
+    mut random_source: ResMut<RandomSource>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let rng = &mut random_source.0;
+
+    let cells: Vec<(Cell, Transform, Entity)> = cell_query
+        .iter()
+        .map(|(cell, transform, entity)| (cell.clone(), transform.clone(), entity))
+        .collect();
+
+    for (cell1, transform1, entity1) in cells.iter() {
+        for (cell2, transform2, entity2) in cells.iter() {
+            if cell1.id == cell2.id || cell1.health < 75.0 || cell2.health < 75.0 {
+                continue;
+            }
+
+            let distance = transform1.translation.distance(transform2.translation);
+            if distance < 50.0 {
+                if rng.gen_bool(cell1.offspring_probability)
+                    && rng.gen_bool(cell2.offspring_probability)
+                {
+                    let offspring = Cell {
+                        parent_1: Some(cell1.id),
+                        parent_2: Some(cell2.id),
+                        id: Uuid::new_v4(),
+                        pos_x: (transform1.translation.x + transform2.translation.x) / 2.0,
+                        pos_y: (transform1.translation.y + transform2.translation.y) / 2.0,
+                        width: (cell1.width + cell2.width) / 2.0 * rng.gen_range(0.9..1.1),
+                        height: (cell1.height + cell2.height) / 2.0 * rng.gen_range(0.9..1.1),
+                        movement_speed: (cell1.movement_speed + cell2.movement_speed) / 2.0
+                            * rng.gen_range(0.9..1.1),
+                        vision_range: (cell1.vision_range + cell2.vision_range) / 2.0
+                            * rng.gen_range(0.9..1.1),
+                        health: 50.0,
+                        attack: (cell1.attack + cell2.attack) / 2.0 * rng.gen_range(0.9..1.1),
+                        target_location: None,
+                        movement_probability: rng.gen(),
+                        offspring_probability: (cell1.offspring_probability
+                            + cell2.offspring_probability)
+                            / 2.0
+                            * rng.gen_range(0.9..1.1),
+                        family_color: blend_colors(
+                            cell1.family_color.to_linear(),
+                            cell2.family_color.to_linear(),
+                            rng,
+                        ),
+                    };
+
+                    commands.spawn((
+                        Mesh2d(meshes.add(Rectangle::new(offspring.width, offspring.height))),
+                        MeshMaterial2d(materials.add(offspring.family_color)),
+                        Transform::default().with_translation(Vec3::new(
+                            offspring.pos_x,
+                            offspring.pos_y,
+                            0.0,
+                        )),
+                        offspring,
+                    ));
+
+                    if let Ok(mut cell1_mut) = cell_query.get_mut(*entity1) {
+                        cell1_mut.0.health -= 25.0;
+                    }
+                    if let Ok(mut cell2_mut) = cell_query.get_mut(*entity2) {
+                        cell2_mut.0.health -= 25.0;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn blend_colors(color1: LinearRgba, color2: LinearRgba, rng: &mut ChaCha12Rng) -> Color {
+    let r = (color1.red + color2.red) / 2.0 * rng.gen_range(0.9..1.1);
+    let g = (color1.green + color2.green) / 2.0 * rng.gen_range(0.9..1.1);
+    let b = (color1.blue + color2.blue) / 2.0 * rng.gen_range(0.9..1.1);
+
+    Color::linear_rgb(r.clamp(0.0, 1.0), g.clamp(0.0, 1.0), b.clamp(0.0, 1.0))
+}
+
+fn draw_gismos(selected_cell: Res<CellSelected>, mut gizmos: Gizmos) {
+    if let Some(cell) = &selected_cell.0 {
+        gizmos.circle_2d(
+            Vec2::new(cell.pos_x, cell.pos_y),
+            cell.vision_range,
+            Color::linear_rgb(1.0, 0.0, 0.0),
+        );
+
+        if let Some(location) = cell.target_location {
+            gizmos.circle_2d(location, 1.0, Color::linear_rgb(1.0, 0.0, 0.0));
         }
     }
 }
